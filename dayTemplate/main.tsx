37cc2ebbbf1992fb25a877f4d4d830d1b3cdc9f0
HeikkiHei <heikkihei.ahonen@gmail.com>
2024-12-02 10:24:26 +0200

    Add empty for day3, make it into a template for copying

---
+++ dayTemplate/main.tsx
@@ -0,0 +1,27 @@
+import { data } from './data.ts';
+import { example } from './example.ts';
+
+// replace data with example to run the example
+const input = data
+    .trim()
+    .split('\n')
+    .map((line) => line.split(' ')
+        .map(Number));
+
+
+function solvePart1(input: number[][]): number {
+    let result = 0;
+    return result;
+}
+
+function solvePart2(input: number[][]): number {
+    let result = 0;
+    return result;
+}
+
+// Run the functions
+const part1Result = solvePart1(input);
+console.log(`Part 1: ${part1Result}`);
+
+const part2Result = solvePart2(input);
+console.log(`Part 2: ${part2Result}`);
