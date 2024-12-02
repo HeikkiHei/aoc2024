12bad20ab565a6fdc45d047aa8ff4b87ba1e86c5
HeikkiHei <heikkihei.ahonen@gmail.com>
2024-12-02 10:15:38 +0200

    Day 2 done Comments for future Heikki

--- day1/main.tsx
+++ day1/main.tsx
@@ -9,13 +9,19 @@ const input = data
     .map(line => line
         .split(/\s+/));;
 
+// split the input into two arrays, one for the left side and one for the right side
 const left = input.map((row) => Number(row.at(0))).sort();
 const right = input.map((row) => Number(row.at(1))).sort();
 
+
+// Calculate the difference between the left and right side and sum them up
 function solvePart1(left: number[], right: number[]): number {
     let result = left
+        // map the left and right side to the difference between the numbers
         .map((l, r) =>
+            // get the absolute difference between the numbers
             Math.abs(Number(l) - Number(right[r])))
+        // sum up the differences
         .reduce(
             (acc, curr) =>
                 acc + curr,
@@ -23,10 +29,14 @@ function solvePart1(left: number[], right: number[]): number {
     return result;
 }
 
+// Multiply the left and right side with the number of times they appear in the other side
 function solvePart2(left: Number[], right: Number[]): number {
     let result = left
+        // map the left side to the number multiplied by the number of times it appears in the right side
         .map((l) =>
+            // get the number of times the number appears in the right side
             Number(l) * right.filter((r) => r === l).length)
+        // sum up the results
         .reduce((
             acc,
             curr,
--- day2/main.tsx
+++ day2/main.tsx
@@ -2,48 +2,76 @@ import { data } from './data.ts';
 import { example } from './example.ts';
 
 // replace data with example to run the example
-const input = example
+const input = data
     .trim()
     .split('\n')
     .map((line) => line.split(' ')
         .map(Number));
 
+// Check each line from the input, if the line is safe, increase the result by 1
 function solvePart1(input: number[][]): number {
     let result = 0;
     for (const line of input) {
-        let [increasing, decreasing, valid] = [true, true, true];
-        
-        for (let i = 1; i < line.length; i++) {
-            if (line[i] > line[i - 1]) {
-                decreasing = false;
-            }
-            if (line[i] < line[i - 1]) {
-                increasing = false;
-            }
-            if (!increasing && !decreasing) {
-                break;
-            }
-        }
-
-        for (let i = 1; i < line.length; i++) {
-            if (Math.abs(line[i] - line[i - 1]) < 1 || Math.abs(line[i] - line[i - 1]) > 3) {
-                valid = false;
-                break;
-            }
-        }
-        
-        if ((increasing || decreasing) && valid) {
+        if (isSafe(line)) {
             result += 1;
         }
-
     }
     return result;
 }
 
+// Check each line from the input, if the line can be made safe, increase the result by 1
 function solvePart2(input: number[][]): number {
     let result = 0;
+    for (const line of input) {
+        if (canBeSafe(line)) {
+            result += 1;
+        }
+    }
     return result;
 }
+
+
+
+// Check if the line is safe, by checking if all the consecutive numbers are increasing or decreasing by 1 or 3
+function isSafe(line: number[]): boolean {
+    let [increasing, decreasing, valid] = [true, true, true];
+
+    for (let i = 1; i < line.length; i++) {
+        // check the difference between the current number and the previous number, if it is inside the threshold of 1 or 3
+        if (Math.abs(line[i] - line[i - 1]) < 1 || Math.abs(line[i] - line[i - 1]) > 3) {
+            valid = false;
+            break;
+        }
+        // check if the numbers are increasing or decreasing
+        if (line[i] > line[i - 1]) {
+            decreasing = false;
+        }
+        if (line[i] < line[i - 1]) {
+            increasing = false;
+        }
+    }
+    // return true if the numbers are increasing or decreasing and the difference between the numbers is valid
+    return (increasing || decreasing) && valid;
+}
+
+function canBeSafe(line: number[]): boolean {
+    // if the line is already safe, no need to check further
+    if (isSafe(line)) {
+        return true;
+    }
+    // if the line is not safe, check if removing any number from the line will make it safe
+    for (let i = 0; i < line.length; i++) {
+        // create a copy of the line without the current number
+        // if this creates a safe line, return true, otherwise just keep on checking
+        const copyArray = [...line.slice(0, i), ...line.slice(i + 1)];
+        if (isSafe(copyArray)) {
+            return true;
+        }
+    }
+    // if it cannot be made safe, return false
+    return false;
+}
+
 
 const part1Result = solvePart1(input);
 console.log(`Part 1: ${part1Result}`);
